a5ef23ffcb90e7e4467e49d5142d38175927b985
mum hrtest <mumhrtest@vistaprint.net>
2018-03-10 13:30:01 +0530

    Sachin test on 10th March 2018

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+	
+	/**
+     * @Route("/list-symfony-github-repos", name="listgithubrepo")
+     */
+    public function listAction(Request $request)
+    {
+		$client = new \Github\Client();		
+		$repositories = $client->api('user')->repositories('symfony');
+
+        // replace this example code with whatever you need
+        return $this->render('default/list.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+			'repositories' => $repositories
+        ));
+    }
+}
